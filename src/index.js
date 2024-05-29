cd05de66792d7a4c619293a4ee5f321d7aa07495
abhishekGhosh99 <125865872+abhishekGhosh99@users.noreply.github.com>
2024-05-30 01:11:07 +0530

    uploaded imports of all files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import HeroBanner from "./components/heroBanner/HeroBanner";
+import ContentWrapper from "./components/contentWrapper/ContentWrapper";
+import SwitchTabs from "./components/switchTabs/SwitchTabs";
+import Carousel from "./components/carousel/Carousel";
+import CircleRating from "./components/circleRating/CircleRating";
+import Genres from "./components/genres/Genres";
 
-import Home from "./pages/Home";
-import Details from "./pages/Details";
-import Explore from "./pages/Explore";
-import PageNotFound from "./pages/PageNotFound";
-import SearchResult from "./pages/SearchResult";
+import Home from "./pages//home/Home";
+import HeroBanner from "./pages/home/heroBanner/HeroBanner";
+import Trending from "./pages/home/trending/Trending";
+import Details from "./pages/details/Details";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Popular from "./pages/home/popular/Popular";
+import TopRated from "./pages/home/topRated/TopRated";
 
 export {
   Header,
   Footer,
+  ContentWrapper,
   Home,
   Details,
   Explore,
   PageNotFound,
   SearchResult,
   HeroBanner,
+  Trending,
+  SwitchTabs,
+  Carousel,
+  CircleRating,
+  Genres,
+  Popular,
+  TopRated,
 };
